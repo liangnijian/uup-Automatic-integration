6e56162d6ed2b9c4524969409d442591f3c0c165
liangnijian <3190776046@qq.com>
2026-03-10 22:25:21 +0800

    Update time-out.cmd

--- bin/time-out.cmd
+++ bin/time-out.cmd
@@ -6,7 +6,7 @@ cls
 echo %h%:%m% 自动运行……
 for /f "tokens=1-2 delims=: " %%a in ("%time%") do set hm=%%a%%b
 if not "%h%%m%"=="%hm%" (
-	timeout /t 60 /nobreak>>nul
+	rem timeout /t 60 /nobreak
+	ping -n 60 127.0.0.1>>nul
 	goto :hm-out
 )
-
