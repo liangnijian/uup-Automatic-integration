e296c940f67037fc94e6237e409f1f7171c3a26f
liangnijian <3190776046@qq.com>
2026-03-10 22:27:52 +0800

    Update download.cmd

--- bin/download.cmd
+++ bin/download.cmd
@@ -6,7 +6,9 @@ echo 正在下载 %filename%
 powershell -command "Invoke-WebRequest -Uri '%uupfile%' -OutFile '%position%'"
 if not exist "%position%" (
 	echo 准备重新下载
-	timeout /t 5 /nobreak
+	rem timeout /t 5 /nobreak
+	ping -n 5 127.0.0.1>>nul
 	goto :download
 )
 goto :EOF
+
